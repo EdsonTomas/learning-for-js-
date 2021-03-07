1dfe9e68e0e1a9743da8f38576977173d81061b6
Edson Tomas <tomasedson1@hotmail.com>
2021-03-07 12:42:10 +0100

    definindo a dimencao do palco do jogo

--- projetos/Jogo mata mosquito/jogo.js
+++ projetos/Jogo mata mosquito/jogo.js
@@ -1,1 +1,9 @@
-alert("Estou aqui")
+
+var altura = 0;
+var largura = 0;
+function ajusteTamanhoDoPalco(){
+	altura = window.innerHeight;
+	largura = window.innerWidth;
+	console.log(altura,largura)
+}
+ajusteTamanhoDoPalco();
